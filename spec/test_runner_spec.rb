37ae311bc5c7943c982c29fb3207cb6a2b1c6ff3
Gordon McCreight <gordon@mccreight.com>
2014-08-29 23:01:23 -0700

    happy path first

--- spec/test_runner_spec.rb
+++ spec/test_runner_spec.rb
@@ -13,17 +13,17 @@ describe Omnimutant::TestRunner do
     runner
   end
 
-  it "should stop a program that runs too long" do
-    runner = example_command_with_timeout("/ruby/example2_spec.rb", 1)
-    assert_equal runner.exceeded_time?, true
-    assert_equal runner.get_result, ""
-  end
-
   it "should get the results from running the command" do
     runner = example_command_with_timeout("/ruby/example1_spec.rb", 3)
     assert_equal runner.exceeded_time?, false
     assert_equal runner.get_result.lines.last.chomp,
       "1 tests, 1 assertions, 0 failures, 0 errors, 0 skips"
+  end
+
+  it "should stop a program that runs too long" do
+    runner = example_command_with_timeout("/ruby/example2_spec.rb", 1)
+    assert_equal runner.exceeded_time?, true
+    assert_equal runner.get_result, ""
   end
 
 end
