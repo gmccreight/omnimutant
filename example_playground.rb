538edce83459ffe102ad0cc34411799cdca9a1b2
Gordon McCreight <gordon@mccreight.com>
2014-09-08 09:21:19 -0700

    add the example playground

---
+++ example_playground.rb
@@ -0,0 +1,23 @@
+# Run this with:
+# ruby -Ilib ./example_playground.rb
+
+require "omnimutant"
+
+base_dir = "/Users/gmccreight/code/mdpg"
+source_dir = base_dir + "/lib"
+
+test_passing_regex =
+  %r{[1-9][0-9]* tests, [1-9][0-9]* assertions, 0 failures, 0 errors, 0 skips}
+
+runner = Omnimutant::Runner.new(
+  dirs:[source_dir],
+  matchers:[%r{search\.rb$}],
+  timeout:5,
+  test_command:"cd #{base_dir}; rake 2>&1",
+  test_passing_regex:test_passing_regex,
+  verbose:true
+)
+
+runner.run()
+
+puts runner.get_report()
